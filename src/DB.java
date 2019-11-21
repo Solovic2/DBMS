b0ce6821cfac725c5cecc42ff15f9849aff2a0e0
Islam Yousry <islamyousry16@gmail.com>
2019-11-21 14:43:45 +0000

    DB.java edited online with Bitbucket

--- src/DB.java
+++ src/DB.java
@@ -1,12 +1,21 @@
 package eg.edu.alexu.csd.oop.db;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Stack;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -15,10 +24,10 @@ import org.w3c.dom.Element;
 import java.util.regex.Pattern;
 
 public class DB implements Database{
+	public final static String XMLFilePath = "C:\\Users\\dell\\Desktop";
 	
-	private Map<String,DocumentBuilder> DBS;
-	
-	
+	private Map<String, Map<String, File>> DBS;
+	private Stack<String> database_names = null;
 	
 	/********************************Singleton Design Pattern********************************/
 	private static DB instance = new DB();
@@ -59,16 +68,13 @@ public class DB implements Database{
 			String DBname = query.substring(16,query.indexOf(";")-1);
 			DocumentBuilderFactory documentfactory = DocumentBuilderFactory.newInstance();
 			try {
-				DBS.put( DBname,documentfactory.newDocumentBuilder()) ;
-			
+				DBS.put( DBname,null) ;
+				database_names.push(DBname);
 			} catch (ParserConfigurationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			DocumentBuilder k = documentfactory.newDocumentBuilder();
-			k.reset();
-			
-			
+				
 		}
 		
 		
@@ -77,46 +83,81 @@ public class DB implements Database{
 		else if(query.contains("drop database")) {
 			String DBname = query.substring(14,query.indexOf(";")-1);
 			if(DBS.containsKey(DBname)) {
-				DBS.get(DBname).reset();
+				Map<String,File> tables = DBS.get(DBname); 
+				File[] files = (File[]) tables.values().toArray();
+				for(int i = 0 ; i < tables.size() ; i++) {
+					files[i].delete();
+				}
 				DBS.remove(DBname);
+				database_names.removeElementAt(database_names.indexOf(DBname));
 			}
-			
-			
 			
 		}
 		
 		
+		
 		else if(query.contains("create table")) {
-			String table_name = query.substring(13,query.indexOf("(")-1);
-			String Cols = query.substring(query.indexOf("(")+1, query.indexOf(")"));
-			String[] cols = Cols.split(",");
-			String[] cols_names = new String[cols.length];
-			String[] cols_dtype = new String[cols.length];
-			int count = 0; 
-			for(String i : cols_dtype) {
-				String temp = "(\\s+)(\\w)(\\s+)(\\w+)";
-				Pattern pattern = Pattern.compile(temp);
-				java.util.regex.Matcher matcher = pattern.matcher(i);
-				if (matcher.matches()) {
-					cols_names[count] = matcher.group(2);
-					cols_dtype[count++]  = matcher.group(4);
-					}
+			if(!database_names.isEmpty()) {
+				String table_name = query.substring(13,query.indexOf("(")-1);
+				String Cols = query.substring(query.indexOf("(")+1, query.indexOf(")"));
+				String[] cols = Cols.split(",");
+				String[] cols_names = new String[cols.length];
+				String[] cols_dtype = new String[cols.length];
+				int count = 0; 
+				for(String i : cols_dtype) {
+					String temp = "(\\s+)(\\w)(\\s+)(\\w+)";
+					Pattern pattern = Pattern.compile(temp);
+					java.util.regex.Matcher matcher = pattern.matcher(i);
+					if (matcher.matches()) {
+						cols_names[count] = matcher.group(2);
+						cols_dtype[count++]  = matcher.group(4);
+						}
+				}
+				DocumentBuilderFactory documentfactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder documentbuilder = null;
+				try {
+					documentbuilder = documentfactory.newDocumentBuilder();
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Document document = documentbuilder.newDocument();
+				
+				Element root = document.createElement(table_name);
+				document.appendChild(root);
+				
+				for(int i = 0 ; i < count ; i++) {
+					Element temp = document.createElement(cols_names[i]);
+					document.appendChild(temp);
+					temp.appendChild(document.createAttribute(cols_dtype[i]));	
+				}
+				
+				
+				
+				TransformerFactory transformerfactory = TransformerFactory.newInstance();
+				Transformer transformer = null;
+				try {
+					transformer = transformerfactory.newTransformer();
+				} catch (TransformerConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				DOMSource domsource = new DOMSource(document);
+				File table_FILE = new File(XMLFilePath+"./"+ table_name +".xml");
+				StreamResult streamresult = new StreamResult(l);
+				
+				
+				try {
+					transformer.transform(domsource,streamresult);
+				} catch (TransformerException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+			
 			}
-			DocumentBuilderFactory documentfactory = DocumentBuilderFactory.newInstance();
-			
-			Document document = DBS.get(key).newDocument();
-			
-			
-			
-			Element root = document.createElement(tagName);
-			document.appendChild(root);
-			
-			for(int i = 0 ; i < count ; i++) {
-				Element temp = document.createElement(cols_names[i]);
-				document.appendChild(temp);
-				temp.appendChild(document.createAttribute(cols_dtype[i]));	
-			}
-			document.removeChild(document);
 			
 		}
 		
