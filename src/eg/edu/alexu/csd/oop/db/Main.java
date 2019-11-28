c807fac78097bf8dee38e09de8eeb5e9506e2e7c
Islam Yousry <islamyousry16@gmail.com>
2019-11-28 21:09:33 +0000

    Main.java edited online with Bitbucket

--- src/eg/edu/alexu/csd/oop/db/Main.java
+++ src/eg/edu/alexu/csd/oop/db/Main.java
@@ -1,22 +1,18 @@
 package eg.edu.alexu.csd.oop.db;
 
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-
-import org.hamcrest.Matcher;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner input=new Scanner(System.in);
-		String s= new String();
-		DB ourinput=DB.get_instance();
-		System.out.println("******************************Welcome TO Our DBMS******************************");
-		System.out.println("Enter Your SQL query in right synatx (to exit just right'end'):");
+		String query = new String();
+		Reload_DATA ourinput = new Reload_DATA();
+		Facade facade = new Facade();
+		System.out.println("*****************************Welcome TO Our DBMS*****************************");
 		try {
 			ourinput.reload();
 		} catch (IOException e1) {
@@ -24,24 +20,21 @@ public class Main {
 			e1.printStackTrace();
 		}
 		while(true) {
-			
-			 s=input.nextLine();
-			 if(s.equalsIgnoreCase("end")) {
+			System.out.println("Enter Your SQL query in right synatx (to exit just right'end'):");
+			 query = input.nextLine();
+			 if(query.equalsIgnoreCase("end")) {
 				 break;
 			 }
-				s=s.toLowerCase();
-				
-							try {
-								ourinput.mainRegex(s);
-							} catch (SQLException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							System.out.println("Enter Your SQL query in right synatx (to exit just right'end'):");
-
+				query = query.toLowerCase();
+						try {
+							facade.do_query(query);
+						} catch (SQLException e) {
+							System.err.println("syntax error");
+							e.printStackTrace();
+						}
 				
 		}
-		System.out.println("******************************Hope You Enjoyed Our DBMS******************************");
+		System.out.println("*****************************Hope You Enjoyed Our DBMS*****************************");
 		
 	
 	}
